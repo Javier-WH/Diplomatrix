dd70532ed976ec7e03a47241af5ded4597e37081
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2024-04-24 10:07:12 -0400

    selector de fuentes mejorado

--- src/renderer/src/components/editText/editTextComponets/fontSelector.jsx
+++ src/renderer/src/components/editText/editTextComponets/fontSelector.jsx
@@ -1,105 +1,94 @@
 
 import { useState, useEffect, useContext } from "react";
-const { ipcRenderer } = window.require('electron');
 import { MainContext } from "../../../contexts/mainContext";
-import NativeFontList from "./nativeFontList";
+import useFontList from "../../../hooks/useFontList";
+import { Button } from 'primereact/button';
 import "./nativeFontlist.css"
+import "./fontSelector.css"
 
 export default function FontSelector() {
 
-  const { elements, selectedElement, addStyle } = useContext(MainContext)
-  const [selectedFont, setSelectedFont] = useState(null);
+  const { selectedElement, addStyle, getStyle, getHeader } = useContext(MainContext)
+  const list = useFontList()
   const [fontList, setFontList] = useState([])
+  const [selectedFont, setSelectedFont] = useState(null);
   const [disabled, setDisabled] = useState(false)
+  const [show, setShow] = useState(false)
 
-
-
+  //agrega la lista de fuentes cuando son recibidas
   useEffect(() => {
-    if (selectedElement === null || disabled) {
+    setFontList(list ?? [])
+  }, [list]);
+
+
+  //agrega el tipo de fuente al texto
+  useEffect(() => {
+    if (selectedElement === null || disabled || getHeader("type") !== "txt") {
+      setShow(false)
       return
     }
-    if (elements[selectedElement]?.header?.type !== 'txt') {
-      return
-    }
-    addStyle({ key: "fontFamily", value: selectedFont?.name })
+    addStyle({ key: "fontFamily", value: selectedFont })
   }, [selectedFont])
 
+  //
   useEffect(() => {
-    if (selectedElement === null) {
+    if (selectedElement === null || getHeader("type") !== "txt") {
       setSelectedFont(null)
       setDisabled(true)
       return
     }
-    if (elements[selectedElement]?.header?.type === 'txt') {
-      const fontName = elements[selectedElement]?.style.fontFamily
-
-      setSelectedFont({
-        name: fontName,
-        code: fontName
-      })
-      setDisabled(false)
-    } else {
-      setSelectedFont(null)
-      setDisabled(true)
-    }
-
+    setSelectedFont(getStyle("fontFamily") ?? "Arial")
+    setDisabled(false)
 
   }, [selectedElement])
 
 
+  const handleClick =(font)=>{
+   setSelectedFont(font)
+   setShow(false)
+  }
 
-  useEffect(() => {
+
+  //evento para cerrar la ventana cuanso se presione escape
+  useEffect(()=>{
+    const cleanShow = (e) =>{
+      if(e.key === "Escape"){
+        setShow(false)
+      }
+
+    }
+    const eventListener = (event) => cleanShow(event)
+
+    if(show){
+      window.addEventListener("keydown", eventListener)
+    }
+
+    return ()=>{
+      window.removeEventListener("keydown", eventListener)
+    }
+  },[show])
 
 
-  }, []);
 
-
-  useEffect(() => {
-    ipcRenderer.send('getFontList');
-  }, []);
-
-  useEffect(() => {
-    const handleFontList = (event, fonts) => {
-
-      const list = fonts.map(font => { return { name: font, code: font } })
-      console.log(list)
-      const nativeList = NativeFontList();
-      const fullList = [
-        ...nativeList,
-        ...(list ?? null),
-      ]
-      setFontList(fullList)
-    };
-
-    ipcRenderer.on('fontsList', handleFontList);
-
-    return () => {
-      ipcRenderer.removeListener('fontsList', handleFontList);
-    };
-  }, []);
-
-  return (
-    <div >
-      <select
-        value={selectedFont?.name ?? ''}
-        onChange={(e) => setSelectedFont(fontList.find(font => font.name === e.target.value))}
-        style={{ width: '300px', height: "45px", fontSize: '1rem' }}
-        disabled={disabled}
-      >
-        {fontList.map(font =>
-          <option
-            key={font.name}
-            value={font.name}
-            style={{ fontFamily: font.name, maxWidth: "300px", fontSize: '1rem', overflow: "hidden" }}
-
+  return <div id="font-list-main-container">
+    <Button label={selectedFont?.replaceAll("\"", "") ?? "Selecciona un Texto"} id="font-list-button" onClick={()=> setShow(!show)} disabled = {disabled}/>
+    <div id="font-list-container" className={show ? "" : "fontHidden"}>
+      {
+        fontList.map(font =>
+          <div 
+            key={font} 
+            onClick={()=>handleClick(font)}
+            className="font-list-fontOpion"
+            style={{ fontFamily: font}}
           >
+            {font.replaceAll("\"", "")}
+          </div>
+        )
+      }
 
-            {font.name !== '"Angel wish"' && font.name !== "Amer" && font.name !== "AmericanTypewriter" ? font.name : null}
-
-
-          </option>)
-        }
-      </select>
     </div>
-  )
+  </div>
+  
 }
+
+
---
+++ src/renderer/src/hooks/useFontList.jsx
@@ -0,0 +1,32 @@
+const { ipcRenderer } = window.require('electron');
+import { useEffect, useState } from 'react';
+import NativeFontList from "./nativeFontList";
+
+const useFontList = () => {
+
+  const [fontList, setFontList] = useState()
+
+  useEffect(() => {
+
+    ipcRenderer.send('getFontList');
+
+    const handleFontList = (event, fonts) => {
+      const nativeList = NativeFontList();
+      const fullList = [
+        ...nativeList,
+        ...(fonts ?? null),
+      ]
+      setFontList(fullList)
+    };
+
+    ipcRenderer.on('fontsList', handleFontList);
+
+    return () => {
+      ipcRenderer.removeListener('fontsList', handleFontList);
+    };
+  }, []);
+
+  return fontList
+};
+
+export default useFontList;
